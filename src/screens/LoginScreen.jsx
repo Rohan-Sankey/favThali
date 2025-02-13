41c4efa9d89d7260be958cae03fc29d9ee220c32
rohan-sankey <rohan.k@sankeysolutions.com>
2025-02-13 11:46:58 +0530

    fixed minor issue and login sreen

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -24,7 +24,7 @@ const LoginScreen = ({navigation}) => {
 
       <TextInput
         style={styles.input}
-        placeholder="Enter Email"
+        placeholder="Email"
         placeholderTextColor="#ccc"
         value={email}
         keyboardType="email-address"
@@ -34,7 +34,7 @@ const LoginScreen = ({navigation}) => {
 
       <TextInput
         style={styles.input}
-        placeholder="Enter Password"
+        placeholder="Password"
         placeholderTextColor="#ccc"
         value={password}
         secureTextEntry
